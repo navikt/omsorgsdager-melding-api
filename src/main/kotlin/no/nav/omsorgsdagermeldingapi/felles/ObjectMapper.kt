fc672e1a2cddac68bc5bfbe74799e655bbe7d136
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-12-11 14:19:34 +0100

    Erstatter deprecated verdier etter oppgradering.

--- src/main/kotlin/no/nav/omsorgsdagermeldingapi/felles/ObjectMapper.kt
+++ src/main/kotlin/no/nav/omsorgsdagermeldingapi/felles/ObjectMapper.kt
@@ -1,21 +1,18 @@
 package no.nav.omsorgsdagermeldingapi.felles
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.*
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.helse.dusseldorf.ktor.jackson.dusseldorfConfigured
 
 val objectMapper: ObjectMapper = jacksonObjectMapper().dusseldorfConfigured()
-    .setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE)
+    .setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE)
     .configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
 
 fun ObjectMapper.k9MellomlagringKonfigurert(): ObjectMapper {
     return jacksonObjectMapper().dusseldorfConfigured().apply {
         configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
-        propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+        propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
     }
 }
 
@@ -23,7 +20,7 @@ fun ObjectMapper.k9SelvbetjeningOppslagKonfigurert(): ObjectMapper {
     return jacksonObjectMapper().dusseldorfConfigured().apply {
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         registerModule(JavaTimeModule())
-        propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+        propertyNamingStrategy = PropertyNamingStrategies.SNAKE_CASE
     }
 }
 
