309bd81e7714a6665d1e46692a20964e63f8dd40
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-04-16 10:54:01 +0200

    Fikser redisHost for redis klient i test

--- src/main/kotlin/no/nav/omsorgsdagermeldingapi/redis/RedisConfig.kt
+++ src/main/kotlin/no/nav/omsorgsdagermeldingapi/redis/RedisConfig.kt
@@ -2,12 +2,14 @@ package no.nav.omsorgsdagermeldingapi.redis
 
 import io.ktor.util.*
 import io.lettuce.core.RedisClient
+import io.lettuce.core.RedisURI
+import java.time.Duration
 
 internal object RedisConfig {
 
     @KtorExperimentalAPI
     internal fun redisClient(redisHost: String, redisPort: Int): RedisClient {
-        return RedisClient.create("redis://${redisHost}:${redisPort}")
+        return RedisClient.create(RedisURI(redisHost, redisPort, Duration.ofSeconds(60)))
     }
 
 }
