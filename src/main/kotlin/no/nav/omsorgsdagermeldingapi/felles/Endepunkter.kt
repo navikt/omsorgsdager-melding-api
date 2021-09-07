5fb4d16272510f569929d6656fe583f03fba827a
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2021-09-07 14:33:32 +0200

    Legger til GET endepunkt for å hente opp vedlegg (#72)
    * Oppdaterer actions, kotlin jvm og gradle wrapper.
    Bytter ut deprecated metoder.
    
    * Closes #71
    
    * Legger til endepunkt for å hente opp vedlegget via api
    
    * Slett ubrukt kode

--- src/main/kotlin/no/nav/omsorgsdagermeldingapi/felles/Endepunkter.kt
+++ src/main/kotlin/no/nav/omsorgsdagermeldingapi/felles/Endepunkter.kt
@@ -4,9 +4,7 @@ const val MELDING_URL_OVERFØRE = "/melding/overforing"
 const val MELDING_URL_FORDELE = "/melding/fordeling"
 const val MELDING_URL_KORONAOVERFØRE = "/melding/koronaoverforing"
 
-
 const val VEDLEGG_URL ="/vedlegg"
-const val SLETTE_VEDLEGG ="/vedlegg/{vedleggId}"
 const val VEDLEGG_MED_ID_URL = "/vedlegg/{vedleggId}"
 
 const val SØKER_URL = "/soker"
